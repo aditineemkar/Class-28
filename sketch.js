8d5203dce48cc20c417086973a6f296da2db261c
Aditi <sneemkar@gmail.com>
2020-12-22 16:28:20 +0530

    created a slinshot

--- sketch.js
+++ sketch.js
@@ -38,8 +38,8 @@ function setup(){
 
     bird = new Bird(100,100);
 
-    log6 = new Log(230,180,80, PI/2);
-    chain = new Chain(bird.body,log6.body);
+    // log6 = new Log(230,180,80, PI/2);
+    slingshot = new Slingshot(bird.body, {x:200,y:100});
 }
 
 function draw(){
@@ -63,6 +63,13 @@ function draw(){
 
     bird.display();
     platform.display();
-    log6.display();
-    chain.display();    
+    // log6.display();
+    slingshot.display();    
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body,{x:mouseX, y:mouseY})
+}
+function mouseReleased(){
+    slingshot.fly();
 }
